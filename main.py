855336224365b9f75a791d7264e2d9210da2cf19
Simon440-png <73174393+Simon440-png@users.noreply.github.com>
2023-03-07 01:18:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,158 @@
+import openai
+import telebot
+from telebot import types
+import config
+
+task = {}
+count = {}
+text = {}
+completion = {}
+
+openai.api_key = config.openai_token
+token = config.telebot_token
+bot = telebot.TeleBot(token)
+
+
+def startgpt(message):
+    data = bot.send_message(message.chat.id, 'Привет, я ChatGPT от OpenAI. Ты можешь задать мне вопрос или просто '
+                                             'поговорить.\nПримеры:\n"Напиши Telegram бота"\n"Что лучше, '
+                                             'Western Digital или Kingston"')
+    bot.register_next_step_handler(data, chatgpt2)
+
+
+def chatgpt(message):
+    keyboard = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton(text="Стоп", callback_data="stopgpt")
+    keyboard.add(button1)
+    data = bot.send_message(message.chat.id, completion[message.chat.id]['choices'][0]['message']['content'], reply_markup=keyboard)
+    bot.register_next_step_handler(data, chatgpt2)
+
+
+def chatgpt2(message):
+    text[message.chat.id] = message.text
+    completion[message.chat.id] = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": text[message.chat.id]}]
+    )
+    print(completion[message.chat.id])
+    chatgpt(message)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    repkey = types.ReplyKeyboardMarkup(row_width=1)
+    gen = types.KeyboardButton('Выбрать AI🤖')
+    rate = types.KeyboardButton('Оставить отзыв о боте🌟')
+    support = types.KeyboardButton('Тех. поддержка👥')
+    repkey.add(gen, rate, support)
+    bot.send_message(message.chat.id, "Привет, это бот выполняющий функции большинства, если не всех продуктов "
+                                      "OpenAI. Это полностью не коммерческий бот "
+                                      "и поддержать разработчика ты можешь подписавшись на boosty, подробнее командой"
+                                      "/pay. Если у вас возникли вопросы по использованию бота, то просмотрите "
+                                      "команду /help или напишите в Тех. поддержку. Внимание! Бот находится на стадии "
+                                      "бета-тестирования", reply_markup=repkey)
+
+
+@bot.message_handler(commands=["help"])
+def helpme(message):
+    bot.send_message(message.chat.id, "Этот бот основан на DALL-E 2 компании openAI и использует официальный API "
+                                      "openAI. Данный бот разработан одним человеком и не приследует коммерческих "
+                                      "целей.\nКоманды бота:\n/pay - поддержать разработчика\n/prompt - сгенерировать "
+                                      "изображение\n/help - вывести это сообщение")
+
+
+@bot.message_handler(commands=['prompt'])
+def prompt(message):
+    data = bot.send_message(message.chat.id, "Введи свой запрос для генерации изображения")
+    bot.register_next_step_handler(data, number)
+
+
+def number(message):
+    task[message.chat.id] = message.text
+    keyboard = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton(text="1️⃣", callback_data="1")
+    button2 = types.InlineKeyboardButton(text="2️⃣", callback_data="2")
+    button3 = types.InlineKeyboardButton(text="3️⃣", callback_data="3")
+    button4 = types.InlineKeyboardButton(text="4️⃣", callback_data="4")
+    keyboard.add(button1, button2)
+    keyboard.add(button3, button4)
+
+    bot.send_message(message.chat.id, "Выберите количество фото для генерации", reply_markup=keyboard)
+
+
+def send_photo(message):
+    global response
+    for i in range(count[message.chat.id]):
+        msg = bot.send_message(message.chat.id, "Генерация изображения...")
+        msg_id = msg.message_id
+        try:
+            response = openai.Image.create(
+                prompt=task[message.chat.id],
+                n=1,
+                size="1024x1024"
+            )
+        except:
+            bot.send_message(message.chat.id, "Ой... Невалидный запрос. Возможно запрос не соответствует политике "
+                                              "безопасности OpenAI")
+        image_url = response['data'][0]['url']
+        bot.delete_message(message.chat.id, message_id=msg_id)
+        bot.send_photo(message.chat.id, image_url)
+    bot.send_message(message.chat.id, "Готово!")
+
+
+@bot.message_handler(commands=["pay"])
+def pay(message):
+    bot.send_message(message.chat.id, "Рад что ты выразил желание помочь, ссылка на boosty: "
+                                      "https://boosty.to/gospodin_moderator. При оформлении платной подписки ты "
+                                      "получишь приятные плюшки, такие как бета-тестирование новых функций, "
+                                      "постоянная связь со мной, доступ к трансляциям, которые я провожу время от "
+                                      "времени и доступ к частному телеграмм чату.")
+
+
+@bot.message_handler(func=lambda call: True)
+def echo_all(message):
+    # Проверяем, на какую кнопку нажал пользователь
+    if message.text == 'Выбрать AI🤖':
+        keyboard = types.InlineKeyboardMarkup()
+        button1 = types.InlineKeyboardButton(text="DALL-E-2", callback_data="dalle")
+        button2 = types.InlineKeyboardButton(text="ChatGPT", callback_data="gpt")
+        keyboard.add(button1, button2)
+        bot.send_message(message.chat.id, "Выбери AI", reply_markup=keyboard)
+    elif message.text == 'Оставить отзыв о боте🌟':
+        bot.send_message(message.chat.id, "Пока в разработке...")
+    elif message.text == 'Тех. поддержка👥':
+        bot.send_message(message.chat.id, "В случае проблем с ботом ты можешь отписать разработчику @sloniko")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    # Обработать нажатие на кнопку
+    if call.data == "1":
+        bot.answer_callback_query(callback_query_id=call.id, text="Вы выбрали 1 картинку")
+        count[call.message.chat.id] = 1
+        send_photo(call.message)
+    elif call.data == "2":
+        bot.answer_callback_query(callback_query_id=call.id, text="Вы выбрали 2 картинки")
+        count[call.message.chat.id] = 2
+        send_photo(call.message)
+    elif call.data == "3":
+        bot.answer_callback_query(callback_query_id=call.id, text="Вы выбрали 3 картинки")
+        count[call.message.chat.id] = 3
+        send_photo(call.message)
+    elif call.data == "4":
+        bot.answer_callback_query(callback_query_id=call.id, text="Вы выбрали 4 картинки")
+        count[call.message.chat.id] = 4
+        send_photo(call.message)
+    elif call.data == "dalle":
+        bot.answer_callback_query(callback_query_id=call.id, text="DALL-E-2 запускается...")
+        prompt(call.message)
+    elif call.data == "gpt":
+        bot.answer_callback_query(callback_query_id=call.id, text="ChatGPT запускается...")
+        startgpt(call.message)
+    elif call.data == "stopgpt":
+        bot.answer_callback_query(callback_query_id=call.id, text="ChatGPT остановлен")
+        start(call.message)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
