1b1f53016e4c37651a26867ddab129c99c20f783
Simon440-png <73174393+Simon440-png@users.noreply.github.com>
2023-06-13 16:45:44 +0200

    Update config.py

--- OpenAI/config.py
+++ OpenAI/config.py
@@ -1,4 +1,4 @@
-openai_token = "My OpenAI token"
+openai_token = "Your OpenAI token"
 telebot_token = "Main Telegram Bot token"
 panel_token = "Panel Telegram Bot token"
 dostup_cost = "Bot access price"
